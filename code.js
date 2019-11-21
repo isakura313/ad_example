96b0b718b10b6a873c01f6f8d406cb39f0284084
Paul <isakura313@gmail.com>
2019-11-21 12:06:42 +0300

    add gallery

--- code.js
+++ code.js
@@ -3,3 +3,17 @@ let icon = document.querySelector("header  .icon");
 icon.onclick = function(){
     nav.classList.toggle("helper");
 }
+
+$(document).ready(function(){
+    $('.columns').magnificPopup({delegate: 'a',
+    type: 'image',
+    gallery :{
+        enabled: true
+    },
+    removalDelay: 300,
+  
+    mainClass: 'mfp-fade'
+  });
+
+
+})
